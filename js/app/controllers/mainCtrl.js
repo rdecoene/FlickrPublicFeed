0a6d3deb2178844a7d98e86219f954b53cb86d2d
Roel Decoene <rdecoene@hotmail.com>
2013-10-06 19:36:01 +0200

    initial commit with basic project

---
+++ js/app/controllers/mainCtrl.js
@@ -0,0 +1,24 @@
+app.controller("MainCtrl", function($scope, $http, $timeout){
+
+	$scope.pictures = []; 
+
+
+	$http.get('http://api.flickr.com/services/feeds/photos_public.gne?tags=potato&tagmode=all&format=json&jsoncallback=?').success(function(data){
+		data = data.replace(/\\'/g, "'"); //Cleaning bad JSON received from Flickr
+		data = data.substring(1);
+		data = data.substring(0, data.length - 1);
+		data = angular.fromJson(data);
+
+		//Clean up data
+		angular.forEach(data.items, function(value, index) {
+			value.author = value.author.substring(19, value.author.length - 1); //Removing nobody@flickr.com
+		});
+
+		//console.log(data);
+		
+		$scope.pictures = data;
+		
+	});
+		
+
+});
