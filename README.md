81d4c4c0ef76cab845247c488f98b68908edeb40
Roel Decoene <rdecoene@hotmail.com>
2013-10-15 20:38:01 +0200

    Solved cross domain restrictions

--- README.md
+++ README.md
@@ -1,16 +1,6 @@
 FLICKR public feed
 ==================
 
-To run local with Google (with OSX):
--------------------------
-
-1. Open Terminal
-2. Go to Applications Folder and type
-
-     `$ open -a Google\ Chrome --args --disable-web-security -–allow-file-access-from-files`
-
-
-
-
 
 Created by Roel Decoene
+http://roeldecoene.be
--- js/controllers/mainCtrl.js
+++ js/controllers/mainCtrl.js
@@ -4,14 +4,7 @@ define(['modules/app', 'filters/tagFilter'], function(app){
 	$scope.pictures = []; 
 	$scope.filtertext = null;
 	
-
-	$http.get('http://api.flickr.com/services/feeds/photos_public.gne?tags=potato&tagmode=all&format=json&jsoncallback=?').success(function(data){
-		data = data.replace(/\\'/g, "'"); //Cleaning bad JSON received from Flickr
-		data = data.substring(1);
-		data = data.substring(0, data.length - 1);
-		data = angular.fromJson(data);
-
-		//Clean up data
+	dataResponse = function(data){
 		angular.forEach(data.items, function(value, index) {
 			value.author = value.author.substring(19, value.author.length - 1); //Removing nobody@flickr.com
 			if (value.author.length > 20) {
@@ -73,8 +66,23 @@ define(['modules/app', 'filters/tagFilter'], function(app){
 		});
 		$scope.pictures = data;
 		sharedProperties.setpics(data); //Put data in custom service
+	};
+
+
+	$http.jsonp('http://api.flickr.com/services/feeds/photos_public.gne?tags=potato&tagmode=all&format=json&jsoncallback=dataResponse').success(function(data){
+
+		
+		//data = data.replace(/\\'/g, "'"); //Cleaning bad JSON received from Flickr
+		//data = data.substring(1);
+		//data = data.substring(0, data.length - 1);
+		//data = angular.fromJson(data);
+
+		//Clean up data
+		
 		
 	});
+
+	
 		
 
 });
